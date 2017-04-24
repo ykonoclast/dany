72eede60df547683dbc8a024c5a6adab299d3f42
ykonoclast <ykonoclast@protonmail.com>
2017-04-24 18:41:28 +0200

    quelques commentaires et un peu de ménage, prêt pour commencer à coder

--- scripts/dany.py
+++ scripts/dany.py
@@ -1,6 +1,8 @@
 import dany_utils
 
 order=input("Entrez un ordre: ")
+
+#vérification initiale du format de la chaîne d'ordre
 passed=dany_utils.check_format(order)
 if passed:
 	print(order+": valid")
--- scripts/dany_utils.py
+++ scripts/dany_utils.py
@@ -1,6 +1,8 @@
+u"""fonctions utilitaires appelées par les autres modules"""
 import re
 
 def check_format(p_order):
+	u"""fonction vérifiant qu'un ordre respecte le format général de Diplomacy, la syntaxe précise et le lexique des provinces n'est pas étudié"""
 	result = False
 	formatStr = re.compile(r"^[AaFf] *[\w]{3} *(t|- *[\w]{3}|[SsCc] *[AaFf] *[\w]{3} *- *[\w]{3}|[Ss] *[AaFf] *[\w]{3}) *$")
 	if formatStr.match(p_order):
