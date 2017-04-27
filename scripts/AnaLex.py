0c34cacf7902343af3a43069d204a7efc2416244
ykonoclast <ykonoclast@protonmail.com>
2017-04-27 22:48:56 +0200

    tokenizer terminé

--- scripts/AnaLex.py
+++ scripts/AnaLex.py
@@ -1,20 +1,28 @@
 from collections import namedtuple
 import re
 
-
 Token = namedtuple('Token', ['categorie', 'valeur'])
-formatStr = re.compile(r"^[AaFf] +[\w]{3} +(t|- +[\w]{3}|[SsCc] +[AaFf] +[\w]{3} +- +[\w]{3}|[Ss] +[AaFf] +[\w]{3})$")
+formatStr = re.compile(r"^[AaFf] +(\w{3}(\([Cc][SsEeNn]\))?) +(t|- +(\w{3}(\([Cc][SsEeNn]\))?)|[SsCc] +[AaFf] +(\w{3}(\([Cc][SsEeNn]\))?) +- +(\w{3}(\([Cc][SsEeNn]\))?)|[Ss] +[AaFf] +(\w{3}(\([Cc][SsEeNn]\))?))$")
+listprov=[]
+				
+def scan_unit(p_scanner,p_lexeme):
+	return Token(categorie='UNITE', valeur=p_lexeme)
 
-def tokenize(p_text):
-	if formatStr.match(p_text):
-		print("match")#provisoire, pour debug
-		scanner=re.Scanner([(r"\b[AaFf]\b", lambda scanner,lexeme:Token(categorie='UNITE', valeur=lexeme)),(r"\b([Cc]|[Ss]| - |[tT])\b", lambda scanner,lexeme:Token(categorie='ORDRE', valeur=lexeme.strip())),(r"\b\w{3}\b", lambda scanner,lexeme:Token(categorie='PROVINCE', valeur=lexeme)),(r" +", None)])
-		
-		results, remainder=scanner.scan(p_text)
-		print(results)
-		print(remainder)
-		
+def scan_prov(p_scanner,p_lexeme):
+	if p_lexeme in listprov:#si la province fait bien partie du vocabulaire
+		return Token(categorie='PROVINCE', valeur=p_lexeme)
 	else:
-		print("pas match")#provisoire, pour debug
-		#TODO exception
+		raise ValueError("refusé, province inconnue")
+	
+def scan_order(p_scanner,p_lexeme):
+	return Token(categorie='ORDRE', valeur=p_lexeme.strip())
 
+def tokenize(p_text,p_conffile):
+	global listprov#on met à jour la liste globale de provinces utilisée par le module
+	listprov = [d['nom'] for d in p_conffile]#crée une liste à partir d'une compréhension de liste obtenue par le parcours du contenu du JSON en ne gardant que les champs "nom"
+	if formatStr.match(p_text):#le format de l'ordre est correct, on peut en extraire les lexèmes pour vérifier leur appartenance au vocabulaire et les tokenizer pour envoi vers l'analyseur syntaxique
+		scanner=re.Scanner([(r"\b[AaFf]\b", scan_unit),(r"\b([Cc]|[Ss]| - |[tT])\b", scan_order),(r"\b(\w{3}(\([Cc][SsEeNn]\))?)\b", scan_prov),(r" +", None)])#tokenization effective via expressions régulières
+		results, remainder=scanner.scan(p_text)
+		return(results)#on jette ce qui n'a pas été matché, à ce stade il ne peut rien s'y trouver d'intéressant car toutes les REGEXP ont matché
+	else:
+		raise ValueError("refusé, format d'ordre incorrect")
--- scripts/dany.py
+++ scripts/dany.py
@@ -2,31 +2,28 @@
 
 import AnaLex
 import argparse
+import json
 
 
-
-#vérification initiale du format de la chaîne d'ordre
-#passed=dany_utils.check_format(order)
-#if passed:
-#	print(order+": valid")
-#else:
-#	print(order+": invalid")
-
-
-if __name__ == '__main__':
-	#gestion des arguments, on peut passer un fichier texte à l'application qui analysera son contenu
-	parser = argparse.ArgumentParser()
-	parser.add_argument("filename",type=str, nargs='?')
-	args = parser.parse_args()
+#gestion des arguments, on peut passer un fichier texte à l'application qui analysera son contenu
+parser = argparse.ArgumentParser()
+parser.add_argument("filename",type=str, nargs='?')
+args = parser.parse_args()
 	
-	if args.filename is not None:
-		#un fichier a été passé en ligne de commande
-		filename=args.filename.strip()
-		with open(filename, "r") as text:
-			for line in text:
-				print(line)#provisoire, pour debug
-				AnaLex.tokenize(line.strip())
-	else:
-		#TODO : mode serveur
-		print("PLACEHOLDER : mode serveur")
+#lecture de la configuration
+with open('../ressources/provinces.json') as data_file:#TODO gérer la position de ce fichier en fonction du lieu d'exécution
+	conffile = json.load(data_file)
+	
+if args.filename is not None:#un fichier a été passé en ligne de commande, on le traite		
+	filename=args.filename.strip()
+	with open(filename, "r") as text:
+		for line in text:	
+			try:
+				tokens=AnaLex.tokenize(line.strip(),conffile)
+				answer = "validé"#l'ordre est passé au tokenizer sans émission d'exception
+			except ValueError as error:
+				answer=str(error)#on enregistre le message d'erreur à destination de l'utilisateur
+			print("{}: {}".format(line.strip(),answer))
+else:#TODO : mode serveur
+	print("PLACEHOLDER : mode serveur")
 	
